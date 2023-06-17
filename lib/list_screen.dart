3942eb48a72e8c384c5bcd76c7674becf088410f
blueknarr <knarrblue@gmail.com>
2023-06-18 00:39:45 +0900

    112. 모델 클래스 만들고 리스트에 표시하기

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/todo.dart';
 
 import 'create_screen.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
   const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final todos = [
+    Todo(
+      title: 'title 1',
+      dateTime: 12321,
+    ),
+    Todo(
+      title: 'title 2',
+      dateTime: 12331,
+    ),
+    Todo(
+      title: 'title 3',
+      dateTime: 12341,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +33,14 @@ class ListScreen extends StatelessWidget {
         title: Text('Todo 리스트'),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subtitle 1'),
-          ),
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subtitle 1'),
-          ),
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subtitle 1'),
-          ),
-        ],
+        children: todos
+            .map(
+              (todo) => ListTile(
+                title: Text(todo.title),
+                subtitle: Text('${todo.dateTime}'),
+              ),
+            )
+            .toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
